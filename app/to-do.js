d3d43761475ef38ecad30f20a78664056ef8c231
Christopher Cahill <cthomascahill@gmail.com>
2019-01-06 20:57:05 -0800

    init

---
+++ app/to-do.js
@@ -0,0 +1,8 @@
+var Task = /** @class */ (function () {
+    function Task(descriptionParameter, priorityParameter) {
+        this.done = false;
+        this.description = descriptionParameter;
+        this.priority = priorityParameter;
+    }
+    return Task;
+}());
